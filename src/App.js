ccb0b7e2c29ba601ef9282f62f06640c10422ddf
Darren Matis <darren.matis@sap.com>
2021-03-13 11:52:34 -0800

    Amount sum now renders in the Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import NumberFormat from "react-number-format";
 import Header from "./Header";
 import Table from "./Table";
 
@@ -10,12 +11,30 @@ import Table from "./Table";
 // handle error cases
 // if time, create a pagination component
 
-// Assume:
-// Columns are based on the values returned in transactions
-
 function App() {
   // Initial data fetch
   const [transactions, setTransactions] = useState([]);
+  const [amount, setAmount] = useState(0);
+
+  // This idea was considered for dynamically generating table headers
+  // but was dropped because the order of the keys from the API is not necessarily
+  // how we would want to display it
+  // function getColumns() {
+  //   if (transactions.length > 0) {
+  //     return Object.keys(transactions[0]);
+  //   }
+  // }
+
+  function sumTransactions() {
+    if (transactions.length > 0) {
+      return transactions
+        .map((t) => parseFloat(t.Amount))
+        .reduce((sum, val) => sum + val)
+        .toFixed(2);
+    }
+    return 0;
+  }
+
   useEffect(() => {
     fetch("https://resttest.bench.co/transactions/1.json")
       .then((res) => res.json())
@@ -24,11 +43,25 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    setAmount(sumTransactions());
+  }, [transactions]);
+
+  const formattedAmount = (
+    <NumberFormat
+      value={amount}
+      displayType={"text"}
+      thousandSeparator={true}
+      prefix={"$"}
+    />
+  );
+
   return (
     <>
       <Header />
       <Table
         columns={["Date", "Company", "Ledger", "Amount"]}
+        columnHeaders={["Date", "Company", "Ledger", formattedAmount]}
         data={transactions}
       />
     </>
--- src/Table.js
+++ src/Table.js
@@ -1,38 +1,38 @@
 import React from "react";
 import "./styles/Table.scss";
 
-// TODO: abstract this so it has no pre-existing awareness of columns
-// pass in a prop called colNames
-function renderTableData({ Date, Ledger, Amount, Company, id }) {
-  return (
-    <tr className="table__row" key={id}>
-      <td className="table__element">{Date}</td>
-      <td className="table__element">{Company}</td>
-      <td className="table__element">{Ledger}</td>
-      <td className="table__element">{Amount}</td>
-    </tr>
-  );
-}
-
-function renderTableHeader(columns = []) {
-  return (
-    <tr className="table__header">
-      {columns.map((col) => (
-        <th className="table__element" key={col}>
-          {col}
-        </th>
-      ))}
-    </tr>
-  );
-}
-
-function Table({ columns, data }) {
+function Table({ columns, columnHeaders, data }) {
   console.log(data);
+
+  function _renderTableData(row, id) {
+    return (
+      <tr className="table__row" key={id}>
+        {columns.map((col) => (
+          <td key={col} className="table__element">
+            {row[col]}
+          </td>
+        ))}
+      </tr>
+    );
+  }
+
+  function _renderTableHeader(columnHeaders = []) {
+    return (
+      <tr className="table__header">
+        {columnHeaders.map((col) => (
+          <th className="table__element" key={col}>
+            {col}
+          </th>
+        ))}
+      </tr>
+    );
+  }
+
   return (
     <table className="table">
       <tbody>
-        {renderTableHeader(columns)}
-        {data.map((row, id) => renderTableData({ ...row, id }))}
+        {_renderTableHeader(columnHeaders)}
+        {data.map((row, id) => _renderTableData(row, id))}
       </tbody>
     </table>
   );
