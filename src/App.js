a32b5c2efc19bc215187044da0eab6432302172d
Darren Matis <darren.matis@sap.com>
2021-03-13 10:15:25 -0800

    Initial draft of the Table component
    Currently displays the first page of data with fixed column names.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
+import Table from "./Table";
+
+// TODO:
+// totalCount contains the number of entries across all pages
+// each page by default has up to 10 entries
+// have a while loop that fetches while there are entries still to fetch
+// handle error cases
+// if time, create a pagination component
+
+// Assume:
+// Columns are based on the values returned in transactions
 
 function App() {
+  // Initial data fetch
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    fetch("https://resttest.bench.co/transactions/1.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setTransactions(data.transactions);
+      });
+  }, []);
+
   return (
     <>
       <Header />
+      <Table
+        columns={["Date", "Company", "Ledger", "Amount"]}
+        data={transactions}
+      />
     </>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,14 +1,38 @@
 import React from "react";
+import "./styles/Table.scss";
 
-function Table({ columns }) {
+// TODO: abstract this so it has no pre-existing awareness of columns
+// pass in a prop called colNames
+function renderTableData({ Date, Ledger, Amount, Company, id }) {
+  return (
+    <tr className="table__row" key={id}>
+      <td className="table__element">{Date}</td>
+      <td className="table__element">{Company}</td>
+      <td className="table__element">{Ledger}</td>
+      <td className="table__element">{Amount}</td>
+    </tr>
+  );
+}
+
+function renderTableHeader(columns = []) {
+  return (
+    <tr className="table__header">
+      {columns.map((col) => (
+        <th className="table__element" key={col}>
+          {col}
+        </th>
+      ))}
+    </tr>
+  );
+}
+
+function Table({ columns, data }) {
+  console.log(data);
   return (
     <table className="table">
       <tbody>
-        <tr>
-          {columns.map((col) => (
-            <th key={col}>{col}</th>
-          ))}
-        </tr>
+        {renderTableHeader(columns)}
+        {data.map((row, id) => renderTableData({ ...row, id }))}
       </tbody>
     </table>
   );
